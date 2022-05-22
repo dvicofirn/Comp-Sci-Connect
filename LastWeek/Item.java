f47061c8f3df8db29bc0109252a01f378203ad3d
dvicofirn <90802825+dvicofirn@users.noreply.github.com>
2022-05-22 20:38:36 +0300

    Create Item.java

---
+++ LastWeek/Item.java
@@ -0,0 +1,29 @@
+import unit4.collectionsLib.*;
+
+public class Item {
+    private int num;
+    private char tav;
+
+    public Item(int num, char tav) {
+        this.num = num;
+        this.tav = tav;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public char getTav() {
+        return tav;
+    }
+
+    public void setTav(char tav) {
+        this.tav = tav;
+    }
+
+
+}
